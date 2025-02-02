b499b9edf294b12e56a0a586d98d5819bf7ee031
Zafrin Chowdhury <zafrinchowdhury@Zafrins-MacBook-Air.local>
2025-02-02 21:31:08 +0600

    Project Setup

---
+++ src/main/java/com/zafrin/chowdhury/Web_Automation_Basics_In_Selenium/App.java
@@ -0,0 +1,13 @@
+package com.zafrin.chowdhury.Web_Automation_Basics_In_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
